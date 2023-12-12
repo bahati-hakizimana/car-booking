dfc0a831d282fd275c8b3f20acbcc58bfe4ab005
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2023-12-12 21:04:38 +0200

    commiting2

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid page-body-wrapper">
+<div class="container-fluid page-body-wrapper bg-gray-300">
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar p-0 fixed-top d-flex flex-row">
       <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
