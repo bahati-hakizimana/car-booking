ecbda760d2d42797df7abf9c04101b98e94db936
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2023-12-12 21:24:06 +0200

    commiting2

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -1,10 +1,10 @@
 <div class="container-fluid page-body-wrappe">
     <!-- partial:partials/_navbar.html -->
-    <nav class="navbar p-0 fixed-top d-flex flex-row">
-      <div class="navbar-brand-wrapper bg-gray-300 d-flex d-lg-none align-items-center justify-content-center">
+    <nav class="navbar bg-gray-300 p-0 fixed-top d-flex flex-row">
+      <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
         <a class="navbar-brand  brand-logo-mini" href="{{url('redirect')}}"><img src="assets/images/Empower.png" alt="logo" /></a>
       </div>
-      <div class="navbar-menu-wrapper bg-gray-300 flex-grow d-flex align-items-stretch">
+      <div class="navbar-menu-wrapper  flex-grow d-flex align-items-stretch">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
           <span class="mdi mdi-menu"></span>
         </button>
